db784f4bed76a16f52386ba73b308a5cdea2a5cd
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-09-19 23:37:57 +0600

    basick set up done

--- js/home.js
+++ js/home.js
@@ -6,3 +6,17 @@ document.getElementById('log-out').addEventListener('click', function(event) {
     // switch to login page
  
 });
+
+
+// set event for cash out in main blanch
+document.getElementById('add-money-btn')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        const mainBalance = getTextFiledById('main-balance');
+        // add balance
+        const addBalance = getInputFiledById('add-balance');
+        const newBalance = mainBalance + addBalance;
+        // update to main balance
+        document.getElementById('main-balance').innerText = newBalance;
+
+    });
---
+++ js/uitilites.js
@@ -0,0 +1,3 @@
+function getInputFiledById (id){
+    
+}
