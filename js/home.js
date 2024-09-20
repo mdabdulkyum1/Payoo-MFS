d448efc602cf5372ed2b615f838929d66dc9f577
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-09-20 10:56:03 +0600

    some validitions added

--- js/home.js
+++ js/home.js
@@ -15,8 +15,17 @@ document.getElementById('add-money-btn')
         const addBalance = getInputFiledById('add-balance');
         const newBalance = mainBalance + addBalance;
         // update to main balance
-        document.getElementById('main-balance').innerText = newBalance;
-        console.log(mainBalance, addBalance)
+        if(!isNaN(addBalance)){
+            document.getElementById('main-balance').innerText = newBalance;  
+        }else{
+            alert("Please Enter Vialed Number");
+        }
+        
+        // clear input filed
+        document.getElementById('add-balance').value = "";
+        document.getElementById('pin').value = "";
+      
+
     });
 // set event for cash out in main blanch
 document.getElementById('cash-out-btn')
@@ -24,13 +33,28 @@ document.getElementById('cash-out-btn')
         event.preventDefault();
         const mainBalance = getTextFiledById('main-balance');
         // add balance
-        const addBalance = getInputFiledById('cash-out-balance');
-        const newBalance = mainBalance - addBalance;
-        // update to main balance
-        document.getElementById('main-balance').innerText = newBalance;
-        console.log(mainBalance, addBalance)
+        const cashOutBalance = getInputFiledById('cash-out-balance');
+        const newBalance = mainBalance - cashOutBalance;
+        if(!isNaN(cashOutBalance) ){
+            // update to main balance
+            if(cashOutBalance > mainBalance){
+                // clear input filed
+                document.getElementById('cash-out-balance').value = "";
+                document.getElementById('cash-pin').value = "";
+                const sms = alert(`No no no, Your balance is: ${mainBalance}`)
+                return sms;
+            }
+            document.getElementById('main-balance').innerText = newBalance;  
+        }else{
+            alert("Please Enter Vialed Number");
+        }
+           // clear input filed
+           document.getElementById('cash-out-balance').value = "";
+           document.getElementById('cash-pin').value = "";
     });
 
+
+    // toggle buttons
     document.getElementById('add-money-action-btn')
     .addEventListener('click', function(){
         hideForm('add-money-form');
